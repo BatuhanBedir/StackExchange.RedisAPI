eda69186b05edd2581fa5bd8e7d2a718c407e0f0
BatuhanBedir <114083704+BatuhanBedir@users.noreply.github.com>
2023-08-21 16:33:47 +0300

    Add project files.

---
+++ RedisExchangeAPI.Web/Controllers/ListTypeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RedisExchangeAPI.Web.Controllers
+{
+    public class ListTypeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ RedisExchangeAPI.Web/Controllers/StringTypeController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RedisExchangeAPI.Web.Services;
+using StackExchange.Redis;
+
+namespace RedisExchangeAPI.Web.Controllers
+{
+    public class StringTypeController : Controller
+    {
+        private readonly RedisService _redisService;
+        private readonly IDatabase db;
+        public StringTypeController(RedisService redisService)
+        {
+            _redisService = redisService;
+            db = _redisService.GetDb(0);
+        }
+
+        public IActionResult Index()
+        {
+            db.StringSet("name", "Batuhan");
+            db.StringSet("visitor",100);
+            return View();
+        }
+        public IActionResult Show()
+        {
+            var value = db.StringLength("name");
+            //var value =db.StringGet("name");
+            //var value = db.StringGetRange("name", 0, 3);
+            //db.StringIncrement("visitor",1);
+
+            //var visitor = db.StringDecrementAsync("visitor", 1).Result;
+            //db.StringDecrementAsync("visitor", 10).Wait();
+
+            //if(value.HasValue)
+            //{
+            //    ViewBag.value = value.ToString();
+            //}
+            ViewBag.value = value.ToString();
+            return View();
+        }
+    }
+}
